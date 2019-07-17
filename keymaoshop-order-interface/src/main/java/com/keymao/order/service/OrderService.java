b725355ebc6ccdca2fcdb3773c30ebb068ce7240
DaMingMing <1044096131@qq.com>
2019-07-18 00:47:56 +0800

    新增注释

--- keymaoshop-order-interface/src/main/java/com/keymao/order/service/OrderService.java
+++ keymaoshop-order-interface/src/main/java/com/keymao/order/service/OrderService.java
@@ -5,6 +5,10 @@ import com.keymao.common.utils.E3Result;
 import com.keymao.order.pojo.OrderInfo;
 
 public interface OrderService {
-
+	/**
+	 * 返回E3Result，包含订单号
+	 * @param orderInfo
+	 * @return
+	 */
 	E3Result createOrder(OrderInfo orderInfo);
 }
